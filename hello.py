7ddd68988a779b828d00028f5f6f2bf8f175fb0d
Emerson Rocha <emersonroocha96@gmail.com>
2024-04-04 00:52:12 -0300

    IMPLEMENTAMOS O HELLO WORLD MULTI LINGUAS

--- hello.py
+++ hello.py
@@ -1,7 +1,40 @@
-#Confiruação do SHEBANG para vários ambientes 
+#!/usr/bin/env python3  
 
-#!/usr/bin/env python3
+"""Hello World multi Linguas.
 
-#Primeiro programa em python
+Dependendo da lingua configurada no ambienteo programa
+exibe a mensagem correspondente.
 
-print("Hello, world".upper())
+
+Como usar:
+
+Tenha a váriavel LANG devidamente configurada, ex:
+
+         export LANG=pt_BR
+
+Execução:
+        python3 hello.py
+        ou
+        ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__ =  "Emerson Rocha"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
