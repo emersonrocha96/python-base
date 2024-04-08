613a777a12f69bee19b3a925034a950e018739a8
Emerson Rocha <emersonroocha96@gmail.com>
2024-04-08 18:40:54 -0300

    Tabuada de x1 a x10

---
+++ emails_formatados.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+email = """
+
+Olá, %(nome)s
+
+Somos da %(empresa)s e gostariamos de saber você teria interesse
+em adiquirir o nosso produto %(produto)s ?
+
+Com ele você poderar manter %(texto)s
+
+Temos apenas %(qtd)d disponiveis, valor promocional de %(preco).2f
+
+Acesse já o nosso site %(link)s e garanta sua vaga!
+"""
+
+clientes = ["Marcos","Felipe", "Emerson"]
+
+for cliente in clientes:
+    print(
+        email %{
+        "nome": cliente,
+        "empresa": "Rocha Seguros 96",
+        "produto": "Seguro Veicular Premium",
+        "texto": """Seu(s) veiculo(s) protegidos 24 horas,
+         gps, alarme e carro reserva""",
+        "qtd": 5,
+        "preco": 250.0,
+        "link": "rochaseguros.com",
+         }
+    )
